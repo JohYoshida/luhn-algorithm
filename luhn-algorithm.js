fced6365c336861e73892db14768af71066312cf
Joh Yoshida <johyoshida@gmail.com>
2017-11-03 21:17:49 +0000

    Added JSdocs

--- luhn-algorithm.js
+++ luhn-algorithm.js
@@ -1,4 +1,9 @@
-
+/**
+ * Checks a number against the Luhn Algorithm
+ * @param  {String} string string containing number you want to check
+ * @return {Null}          returns null if "string" is not a string
+ * @return {Bool}          returns true if number passes Luhn Algorithm
+ */
 function check(string) {
   if (typeof string !== "string") {
     return null;
