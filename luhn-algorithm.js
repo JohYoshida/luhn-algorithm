0acd03c75a8cc554dfe3bc9648151dfea25f2a00
Joh Yoshida <johyoshida@gmail.com>
2017-11-03 21:11:36 +0000

    Implemented Luhn algorithm

--- luhn-algorithm.js
+++ luhn-algorithm.js
@@ -1,6 +1,25 @@
 
-function check(number) {
-  return null;
+function check(string) {
+  if (typeof string !== "string") {
+    return null;
+  }
+  // add last digit to sum
+  var sum = Number(string[string.length-1]);
+
+  var numDigits = string.length;
+  var parity = numDigits % 2;
+  for (var i = numDigits - 2; i >= 0; i--) {
+    var digit = Number(string[i]);
+    if (i % 2 === parity) {
+      digit = digit * 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+    sum += digit;
+  }
+  return (sum % 10 === 0);
 }
+console.log(check("aaa"));
 
 module.exports = check;
